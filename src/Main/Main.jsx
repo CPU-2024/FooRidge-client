941274c2e84b75129cd42d89b7d86a683d97e235
GoEun <w2219@e-mirim.hs.kr>
2024-02-23 06:19:23 +0900

    Main 최근 게시물, 위치 수정

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -3,31 +3,36 @@ import logo from '../assets/logo.png'; // 이미지 파일 경로를 맞게 수�
 import about from '../assets/About.png';
 import share from '../assets/share.png';
 import low_price from '../assets/low price.png';
+import dunkin from '../assets/dunkin.png';
+import bagette from '../assets/bagette.png';
+import hamberger from '../assets/hamberger.png';
 import { useNavigate } from 'react-router-dom';
 import '../Main/Main.css';
 
 export default function Main() {
   const navigate = useNavigate();
 
-  const handleSignInClick = () => {
-    navigate('/login'); // 로그인 페이지로 이동
-  };
-
-  const handleSignUpClick = () => {
-    navigate('/signup'); // 회원가입 페이지로 이동
+  const handleMore = () => {
+    navigate(''); // 회원가입 페이지로 이동
   };
 
   return (
     <div>
+      <h2>FOORIDGE</h2>
         <div className='page'>
-            <h2>FOORIDGE</h2>
-            <About />
-            <div className="button">
-                <Share /><Low_Price />
-            <div className="post">
-                <h3>최근에 올라온 글</h3>
-            </div>
-            </div>
+              <div className='adver'>
+                <About />
+              </div>
+              <div className='button'>
+                  <img src={share} alt="Share" />
+                  <img src={low_price} alt="Low_Price" />
+              </div>
+              <div className='post'>
+                  <h3>최근에 올라온 글</h3>
+              </div>
+              <div className='more'>
+                  <h5 onClick={handleMore}>더보기</h5>
+              </div>
         </div>
     </div>
   );
@@ -42,20 +47,3 @@ function About() {
   );
 }
 
-function Share() {
-    return (
-      <div className='Share'>
-        {/* 이미지를 추가합니다 */}
-        <img src={share} alt="Share" />
-      </div>
-    );
-}
-
-function Low_Price() {
-    return (
-      <div className='Low_Price'>
-        {/* 이미지를 추가합니다 */}
-        <img src={low_price} alt="Low_Price" />
-      </div>
-    );
-}
