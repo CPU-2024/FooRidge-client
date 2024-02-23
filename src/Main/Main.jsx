08595ef1b0684549ebaae281b467ab49afcd682b
GoEun <w2219@e-mirim.hs.kr>
2024-02-23 19:33:28 +0900

    Main 하단바 게시물, css 수정

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -6,6 +6,11 @@ import low_price from '../assets/low price.png';
 import dunkin from '../assets/dunkin.png';
 import bagette from '../assets/bagette.png';
 import hamberger from '../assets/hamberger.png';
+import home from '../assets/carbon_home.png';
+import search from '../assets/tabler_search.png';
+import plus from '../assets/plus.png';
+import chat from '../assets/chat.png';
+import profile from '../assets/profile.png';
 import { useNavigate } from 'react-router-dom';
 import '../Main/Main.css';
 
@@ -27,11 +32,23 @@ export default function Main() {
                   <img src={share} alt="Share" />
                   <img src={low_price} alt="Low_Price" />
               </div>
-              <div className='post'>
+              <div className='recen'>
                   <h3>최근에 올라온 글</h3>
               </div>
               <div className='more'>
                   <h5 onClick={handleMore}>더보기</h5>
+              </div>
+              <div className='post'>
+                  <img src={dunkin} alt="" />
+                  <img src={bagette} alt="" />
+                  <img src={hamberger} alt="" />
+              </div>
+              <div className='bottom_bar'>
+                  <img src={home} alt="" />
+                  <img src={search} alt="" />
+                  <img src={plus} alt="" />
+                  <img src={chat} alt="" />
+                  <img src={profile} alt="" />
               </div>
         </div>
     </div>
