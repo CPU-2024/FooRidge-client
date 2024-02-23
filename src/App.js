2fe13d44323f262f1cc86ac21df9d347aad5b109
honghyobin <w2234@e-mirim.hs.kr>
2024-02-23 16:13:53 +0900

    [feat] 나눔하기

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import SignupSignin from './Signinup/SignupSignin';
 import Signin from './Signin/Signin';
 import Location from './Location/location';
 import Main from './Main/Main';
-
+import Post from './Post/Post';
 function App() {
   return (
     <Router>
     <Routes>
-      <Route path="/" element={<SignupSignin />} />
+      {/* <Route path="/" element={<SignupSignin />} />
       <Route path="/login" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
       <Route path='/location' element={<Location/>}/>
-      <Route path='/main' element={<Main/>}/>
+      <Route path='/main' element={<Main/>}/> */}
+      <Route path='/post' element={<Post/>}/>
     </Routes>
   </Router>
   // <Location />
