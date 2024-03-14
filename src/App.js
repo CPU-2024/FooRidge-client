8cef50056aedf4827ee746e584816180b479fddf
fire-irror <firenoah.m@gmail.com>
2024-03-15 01:00:02 +0900

    fix: 회원가입, 홈, 로그인 연동

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; // Navigate 추가
 import Signup from './Signup/Signup';
 import SignupSignin from './Signinup/SignupSignin';
 import Signin from './Signin/Signin';
 import Location from './Location/location';
 import Main from './Main/Main';
 import Post from './Post/Post';
+
 function App() {
   return (
     <Router>
-    <Routes> 
-      <Route path="/" element={<SignupSignin />} />
-      <Route path="/login" element={<Signin />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path='/location/:userId' element={<Location/>}/>
-      <Route path='/main' element={<Main/>}/>
-      <Route path='/post' element={<Post/>}/>
-    </Routes>
-  </Router>
-  // <Location />
+      <Routes>
+        <Route path="/" element={<SignupSignin />} />
+        <Route path="/login" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path='/location/:userId' element={<Location/>}/>
+        <Route path='/main' element={<Main/>}/>
+        <Route path='/post' element={<Post/>}/>
+      </Routes>
+    </Router>
   );
 }
 
